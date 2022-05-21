224d18d0afa54dd40ea60aa5792238bd6ec87cdf
Marcelo Colla <marcelo.colla@contasimples.com>
2022-05-21 19:58:32 -0300

    configurando as rotas de solicitacoes

--- src/app/RoutesMap.ts
+++ src/app/RoutesMap.ts
@@ -4,11 +4,20 @@ import { RouteProps } from 'react-router-dom'
 
 const HomePageLazy = React.lazy(() => import('pages/Home'))
 
-export const getRoutesMap = (basename = ''): RouteProps[] => {
+export const getRoutesMap = (basename = '/solicitacoes'): RouteProps[] => {
   return [
     {
       exact: true,
-      path: `${basename}/`,
+      path: basename,
+      component: HomePageLazy,
+    },
+    {
+      exact: true,
+      path: `/solicitacoes/:id`,
+      component: HomePageLazy,
+    },
+    {
+      path: '*',
       component: HomePageLazy,
     },
   ]
