29ee0e71c073c282499f710fef9512cc8e12b3ea
Marcelo Colla <marcelo.colla@contasimples.com>
2022-11-20 11:41:58 -0300

    ajuste para otimizar resultados de analise

--- src/components/ApprovalList/ApprovalList.tsx
+++ src/components/ApprovalList/ApprovalList.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { Card } from '@utfprfabricadesoftware/utfpr-lib-ui-react'
 
-import { LiberacaoProps } from 'types/Liberacao'
 import httpClient from 'services/httpClient'
+import { toLocaleString } from 'helpers/dateFormat'
+import { LiberacaoProps } from 'types/Liberacao'
+
 import { BoxList } from './ApprovalList.styles'
 
 export const ApprovalList = React.memo(() => {
   const history = useHistory()
 
   const [liberacoes, setLiberacoes] = React.useState<LiberacaoProps[]>([])
+  const data = liberacoes || []
 
   // Recupera as liberações ativas, ou seja, aquelas cuja data
   // final de liberação ainda está válida (data_fim >= hoje).
@@ -23,23 +26,30 @@ export const ApprovalList = React.memo(() => {
     }
   }
 
-  React.useEffect(() => {
+  function getPersonName(item: LiberacaoProps) {
+    try {
+      return item.Aluno.Pessoa.nome_pessoa
+    } catch (err) {
+      return '---'
+    }
+  }
+
+  useEffect(() => {
     loadApprovals()
   }, [])
 
   return (
     <BoxList>
-      {liberacoes &&
-        liberacoes.map((item) => (
-          <Card
-            key={item.id_liberacao}
-            name={item.Aluno?.Pessoa?.nome_pessoa || '---'}
-            leftInfo={'De: ' + new Date(item.data_inicio).toLocaleDateString('pt-BR')}
-            rightInfo={'Até: ' + new Date(item.data_fim).toLocaleDateString('pt-BR')}
-            removeDisabled={true}
-            onEdition={() => history.push('/liberacoes/' + item.id_liberacao)}
-          />
-        ))}
+      {data.map((item) => (
+        <Card
+          key={item.id_liberacao}
+          name={getPersonName(item)}
+          leftInfo={'De: ' + toLocaleString(item.data_inicio)}
+          rightInfo={'Até: ' + toLocaleString(item.data_fim)}
+          removeDisabled={true}
+          onEdition={() => history.push('/liberacoes/' + item.id_liberacao)}
+        />
+      ))}
     </BoxList>
   )
 })
